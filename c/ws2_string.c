e93318ad8241d8e61cee181e75f7633ad909f322
ido.finkelstein <finkelsteinido@gmail.com>
2020-05-05 15:50:19 +0300

    update file

--- c/ws2_string.c
+++ c/ws2_string.c
@@ -1,13 +1,45 @@
+/*********************** 
+file name: ws2_string.c
+exersice: strings
+author: Ido Finkelstein
+date: 5/5/20
+reviewer: 
+************************/
+#include <stdio.h> /* size_t */
 
 size_t StrLen(const char *str)
 {
-	size_t length = 0;
+	char *current_member = (char*)str;
 	
-	while (*str != '\0')
+	while ('\0' != *current_member)
 	{
-		++length;
-		++str;
+		++current_member;
 	}
 
-	return (length);
+	return (current_member - str);
+}
+
+int StrCmp(const char *str1, const char *str2)
+{
+	/*char *current_str1 = (char*)str1, *current_str2 = (char*)str2;*/
+
+	while ('\0' != *str1 || '\0' != *str2)
+	{
+		if (*str1 == *str2)
+		{
+			++str1;
+			++str2;
+		}
+		else if(*str1 < *str2)
+		{
+			return (*str1-*str2);
+		}
+		else
+		{
+			return (*str1-*str2);
+		}
+	}
+	
+	return (*str1-*str2);	
+
 }
