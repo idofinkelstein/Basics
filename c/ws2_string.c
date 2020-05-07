2bf6e10a5b0f4babb9dfc66fdc9ec0c713773261
ido.finkelstein <finkelsteinido@gmail.com>
2020-05-07 12:28:08 +0300

    updating strstr

--- c/ws2_string.c
+++ c/ws2_string.c
@@ -219,51 +219,44 @@ char *StrnCat(char *dest, const char *src, size_t n)
 
 char *StrStr(const char *haystack, const char *needle)
 {
-	size_t i = 0, j = 0, k = 0, cnt = 0;
+	size_t i = 0, j = 0, cnt = 0;
 	size_t hay_length = StrLen(haystack);
 	size_t nee_length = StrLen(needle);
 	char *curr = (char*)haystack;
-	char *tmp_curr = (char*)haystack;
+	char *first = (char*)haystack;
 
 	assert(haystack);
 	assert(needle);
 
 	for (i = 0; i < hay_length; ++i)
 	{
-		curr = curr + i;
 		cnt = 0;
 
 		for(j = i; j < hay_length; ++j)
 		{
-			if (*(haystack + j) == *(needle + k))
+			if (*(haystack + j) == *(needle + cnt))
 			{
 				++cnt;
 
-				if (cnt == 1)
+				if (1 == cnt)
 				{
 					curr += j;
-				}
-
-				k++;
+				}	
 			}
 			else
 			{
-				k = 0;
 				cnt = 0;
-				curr = tmp_curr;
-				
+				curr = first;
 			}
 
 			if (cnt == nee_length)
 			{
 				return (curr);
 			}
-			
 		}
 	}
 
 	return (curr = NULL);
 }
-
 
 
