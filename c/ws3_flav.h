02c6e47e7013bb59531750591a6358c84ee45e15
ido.finkelstein <finkelsteinido@gmail.com>
2020-05-13 10:07:25 +0300

    final version of flav.h

--- c/ws3_flav.h
+++ c/ws3_flav.h
@@ -5,6 +5,6 @@ int Flav(unsigned n);
 
 void InitArray(int arr[], unsigned n);
 
-void Kill(int arr[], int n, int servivors, int *kill, int *winner);
+void Kill(int arr[], unsigned n);
 
 #endif /* __WS3_FLAV_H__ */
