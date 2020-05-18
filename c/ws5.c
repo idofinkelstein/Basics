4bba468486c2dc529c432b91f1c54d9f36fa6977
ido.finkelstein <finkelsteinido@gmail.com>
2020-05-18 20:40:06 +0300

    final version

--- c/ws5.c
+++ c/ws5.c
@@ -11,163 +11,206 @@ date: 18.5.2020
 #include <string.h> /* strcmp, strlen                	   */
 #include <assert.h>
 
-#define UNUSED (void) argv
+#define UNUSED (void) argc
 
-#define APPEND 0
-#define REMOVE 1
-#define COUNT 2
-#define WRITE 3
-#define QUIT 4
+#define TRUE 1
+#define FALSE 0
 #define SIZE 5
+#define MAX_LINE_SIZE 250
 
-enum STATUS {SECCESS, OPEN_FAILED};
- 
-int Comparison(char*);
+enum STATUS {SECCESS = 0, FAILED};
+
+/* comperison functions declaretion */
+int AppendComparison(char*);
+int RemoveComparison(char*);
+int CountComparison(char*);
+int WriteComparison(char*);
+int QuitComparison(char*);
 
 /* operation functions declaretion */
-enum STATUS Append(char*, int, char *name);
-enum STATUS Remove(char*, int, char *name);
-enum STATUS Count(char*, int, char *name);
-enum STATUS Write(char*, int, char *name);
-enum STATUS Quit(char*, int, char *name);
+enum STATUS Append(char *line_input, char *file_name);
+enum STATUS Remove(char *line_input, char *file_name);
+enum STATUS Count(char *line_input, char *file_name);
+enum STATUS Write(char *line_input, char *file_name);
+enum STATUS Quit(char *line_input, char *file_name);
 
 
 struct what_to_do
 {
- 	char *str;
+ 	char *cmpr_input;
 	int (*cmpr_ptr)(char*);
-	enum STATUS (*opr_ptr)(char*, int, char *name);
+	enum STATUS (*opr_ptr)(char*, char*);
+
 }arr[SIZE];
 
 /*initializing struct what_to_do variables */
-struct what_to_do append = {"", Comparison, Append};
-struct what_to_do delete = {"-remove\n", Comparison, Remove};
-struct what_to_do count = {"-count\n", Comparison, Count};
-struct what_to_do write_to_start = {"<", Comparison, Write};
-struct what_to_do quit = {"-exit\n", Comparison, Quit};
+struct what_to_do append = {"", AppendComparison, Append};
+struct what_to_do delete = {"-remove\n", RemoveComparison, Remove};
+struct what_to_do count = {"-count\n", CountComparison, Count};
+struct what_to_do write_to_start = {"<", WriteComparison, Write};
+struct what_to_do quit = {"-exit\n", QuitComparison, Quit};
 
 
-int main(int argv, char **argc)
+int main(int argc, char **argv)
 {
-	int status = 0; /* for recieving output from compare function
-					   and to know which operation function to
-					   refer to */
-	char s[100] = "";
+	char line_input[MAX_LINE_SIZE] = "";
+	char *file_name = argv[1];
 	int i = 0;
 	
-	arr[0] = append;
+	arr[0] = quit;
 	arr[1] = delete;
 	arr[2] = count;
 	arr[3] = write_to_start;
-	arr[4] = quit;
+
+	/* future features should be placed here */
+
+	arr[4] = append;
 
 	UNUSED;
 
-	printf("enter text to to file\nfor exit type -exit and press Enter"
+	printf("enter text to file\nfor exit type -exit and press Enter"
 		   "\nfor outputing number of lines type -count"
-		   " end press enter\nfor writting the first line"
+		   " and press enter\nfor writting the first line"
 		   " type < before the text.\n");
 
-	while (1)
+	while (TRUE)
 	{
-		fgets(s, 100, stdin);
+		fgets(line_input, MAX_LINE_SIZE, stdin);
+
 		for(i = 0; i < SIZE; ++i)
 		{
-			status = arr[i].cmpr_ptr(s);
-			arr[i].opr_ptr(s, status, argc[1]);	
+			if (TRUE == arr[i].cmpr_ptr(line_input))
+			{
+				arr[i].opr_ptr(line_input, file_name);
+
+				break;
+			}	
 		}
 	}
-	return 0;
+	return (SECCESS);
 }
 
-/* compares input string to operation string */
-int Comparison(char* str)
+int AppendComparison(char* line_input)
 {
-	assert(str);
+	assert(line_input);
+	
+	(void)line_input;
 
-	if (strcmp(quit.str, str) == 0)
+	return (TRUE);
+}
+
+int RemoveComparison(char* line_input)
+{
+	assert(line_input);
+ 
+	if (strcmp(delete.cmpr_input, line_input) == 0)
 	{
-		return QUIT;
-	}
-	else if (strcmp(count.str, str) == 0)
-	{
-		return COUNT;
-	}
-	else if (write_to_start.str[0] == str[0])
-	{
-		return WRITE;
-	} 
-	else if (strcmp(delete.str, str) == 0)
-	{
-		return REMOVE;
-	}
-	else
-	{
-		return APPEND;
+		return (TRUE);
 	}	
+
+	return (FALSE);
 }
 
-/* appends text to the end of the file given by the user */
-enum STATUS Append(char* str, int status, char *name)
+int CountComparison(char* line_input)
 {
-	assert(str);	
+	assert(line_input);
 
-	if(status == APPEND && name != NULL)
+	if (strcmp(count.cmpr_input, line_input) == 0)
 	{
-		FILE *file_ptr = fopen(name, "a"); 
-
-		fwrite(str, sizeof(char), strlen(str), file_ptr);
-		fclose(file_ptr);
-
-		return SECCESS;			
-	}
-
-	if (status == APPEND && name == NULL)
-	{
-		printf("file name was not entered\n");
+		return (TRUE);
 	}
 	
-	return OPEN_FAILED;	
+	return (FALSE);	
+}
+
+int WriteComparison(char* line_input)
+{
+	assert(line_input);
+
+	if (write_to_start.cmpr_input[0] == line_input[0])
+	{
+		return (TRUE);
+	} 
+	
+	return (FALSE);	
+}
+
+int QuitComparison(char* line_input)
+{
+	assert(line_input);
+
+	if (strcmp(quit.cmpr_input, line_input) == 0)
+	{
+		return (TRUE);
+	}
+	
+	return (FALSE);
+	
+}
+/* appends text to the end of the file given by the user */
+enum STATUS Append(char* line_input, char *file_name)
+{
+	assert(line_input);	
+
+	if(NULL != file_name)
+	{
+		FILE *file_ptr = fopen(file_name, "a");
+ 
+		if(NULL != file_ptr)
+		{
+			fwrite(line_input, sizeof(char), strlen(line_input), file_ptr);
+		}
+
+		if(SECCESS == fclose(file_ptr))
+		{
+			return (SECCESS);
+		}
+
+		return (FAILED);	
+	}
+
+	printf("file name was not entered\n");
+	
+	return (FAILED);	
 }
 
 /* deletes the file */
-enum STATUS Remove(char* str, int status, char *name)
+enum STATUS Remove(char* line_input, char *file_name)
 {
-	assert(str);
+	assert(line_input);
 	
-	if (status == REMOVE)	
-	{
-		(void)str;
-		(void)status;
+	(void)line_input;
 		
-		if (NULL != name)
-		{
-			remove(name);
-			return SECCESS;
-		}
+	if (NULL != file_name)
+	{
+		remove(file_name);	
+
+		return (SECCESS);
 	}
 	
-	return OPEN_FAILED;
+	printf("no file to remove\n");	
+
+	return (FAILED);
 }
 
 /* counts number of lines */
-enum STATUS Count(char* str, int status, char *name)
+enum STATUS Count(char* line_input, char *file_name)
 {
-	assert(str);
+	assert(line_input);
 
-	if (status == COUNT && name != NULL)
+	if (NULL != file_name)
 	{
 		char c = '\0';
 		int cnt = 0;
-		FILE *file_ptr = fopen(name, "r");
+		FILE *file_ptr = fopen(file_name, "r");
 
-		(void)str;
+		(void)line_input;
 
 		if(NULL == file_ptr)
 		{
 			printf("failure, no such file\n");
 		
-			return OPEN_FAILED;
+			return (FAILED);
 		}
 
 		while((c = fgetc(file_ptr)) && c != EOF)
@@ -177,47 +220,56 @@ enum STATUS Count(char* str, int status, char *name)
 				++cnt;
 			}
 		}				
-			
-		fclose(file_ptr);
-
-		printf("nuber of lines: %d\n", cnt);
-
-		return SECCESS;
+		printf("nuber of lines: %d\n", cnt);	
+		
+		if(SECCESS == fclose(file_ptr))
+		{
+			return (SECCESS);
+		}
+		
+		return (FAILED);
+		
 	}
 
-	if (status == COUNT && name == NULL)
-	{
-		printf("file name was not entered\n");
-	}
+	printf("file name was not entered\n");
 	
-	return OPEN_FAILED;	
+	return (FAILED);	
 }
 
 /* writes to the beginning of the file */
-enum STATUS Write(char* str, int status, char *name)
+enum STATUS Write(char* line_input, char *file_name)
 {
-	assert(str);
+	assert(line_input);
 
-	if (status == WRITE && name != NULL)
+	if (NULL != file_name)
 	{
-		FILE *file_ptr = fopen(name, "r");
+		FILE *file_ptr = fopen(file_name, "r");
 		FILE *tmp_file_ptr = NULL;
 		char c = '\0';
 	
-		++str;
+		++line_input;
 
-		/* creates temporery file and writes the input
-		   to the head of the file */
 		if(NULL != file_ptr)
 		{
+		 /* creates temporery file and writes the input
+	        to the head of the file */
 			tmp_file_ptr = fopen("tmp_txt", "a");
-			fwrite(str, sizeof(char), strlen(str), tmp_file_ptr);
+
+			if (NULL != tmp_file_ptr)
+			{
+				fwrite(line_input, sizeof(char), strlen(line_input), tmp_file_ptr);
+			}
+			else
+			{
+				return (FAILED);
+			}
+
 		}
 		else
 		{
 			printf("the file does not exist\n");
 		
-			return OPEN_FAILED;
+			return FAILED;
 		}
 	
 		/*appends the original file to temp file */
@@ -226,38 +278,33 @@ enum STATUS Write(char* str, int status, char *name)
 			fputc(c, tmp_file_ptr);
 		}
 
-		fclose(file_ptr);
-		fclose(tmp_file_ptr);
+		if (SECCESS == fclose(file_ptr) && SECCESS == fclose(tmp_file_ptr))
+		{
+			remove(file_name);
 
-		remove(name);
+			rename("tmp_txt", file_name);
 
-		rename("tmp_txt", name);
+			return SECCESS;
+		}
 
-		return SECCESS;
+		remove(file_name);
+
+		rename("tmp_txt", file_name);
+
+		return FAILED;
 	}
 	
-	if (status == WRITE && name == NULL)
-	{
-		printf("file name was not entered\n");
-	}
+	printf("file name was not entered\n");	
 	
-		return OPEN_FAILED;	
+	return FAILED;	
 }
 
 /* exits the program */
-enum STATUS Quit(char* str, int status, char *name)
+enum STATUS Quit(char* line_input, char *file_name)
 {
-	if (status == QUIT)
-	{
-		(void)str;
-		(void)status;
-		(void)name;
+		(void)line_input;
+		(void)file_name;
 
 		exit(0);
-	}
-	else
-	{
-		return OPEN_FAILED;
-	}
 }
 
