7907aca3af022139003b0300ab5ea523d14c157e
ido.finkelstein <finkelsteinido@gmail.com>
2020-06-03 17:59:36 +0300

    updated

---
+++ ds/ilrd_bit_array.c
@@ -0,0 +1,230 @@
+/*********************
+File name: bit_array.c
+Author: Ido finkelsein
+Reviewer: Uriel Naiman
+Date: 2/6/2020
+**********************/
+
+#include <stdio.h>
+#include <stddef.h> /* size_t */
+
+#define NUM_OF_BIT (sizeof(size_t) * 8)
+#define WORD_SIZE sizeof(size_t)
+#define CHAR_VALS 256
+#define MAX_SHIFT (sizeof(size_t) * 8 - 1)
+
+static char bit_lut_g[CHAR_VALS] = {0};
+static unsigned char mirror_lut_g[CHAR_VALS] = {0};
+
+typedef size_t bit_array_t; 
+
+bit_array_t BitArrSetAll(bit_array_t arr)
+{
+	return (arr = ~0);
+}
+
+bit_array_t BitArrResetAll(bit_array_t arr)
+{
+	return (arr = 0);
+}
+
+bit_array_t BitArrSetOn(bit_array_t arr, size_t index)
+{
+	return (arr | ((size_t)1 << (index - 1)));
+}
+
+bit_array_t BitArrSetOff(bit_array_t arr, size_t index)
+{
+	return (arr & (~((size_t)1 << (index - 1))));
+}
+
+bit_array_t BitArrSetBit(bit_array_t arr, size_t index, int boolean_value)
+{
+	return(boolean_value ? BitArrSetOn(arr, index) : BitArrSetOff(arr, index));
+}
+
+bit_array_t BitArrFlip(bit_array_t arr, size_t index)
+{
+	return (arr ^ ((size_t)1 << (index - 1)));
+}
+
+int BitArrIsOn(bit_array_t arr, size_t index)
+{
+	return (!!(arr & ((size_t)1 << (index - 1))));
+}
+
+int BitArrIsOff(bit_array_t arr, size_t index)
+{
+	return (!(arr & ((size_t)1 << (index - 1))));
+}
+
+bit_array_t BitArrRotR(bit_array_t arr, size_t num_to_shift)
+{
+	if (num_to_shift >= NUM_OF_BIT)
+	{
+		num_to_shift %= NUM_OF_BIT;
+	}
+
+	return ((arr >> num_to_shift) | (arr << (NUM_OF_BIT - num_to_shift)));
+}
+
+bit_array_t BitArrRotL(bit_array_t arr, size_t num_to_shift)
+{
+	if (num_to_shift >= NUM_OF_BIT)
+	{
+		num_to_shift %= NUM_OF_BIT;
+	}
+
+	return ((arr << num_to_shift) | (arr >> (NUM_OF_BIT - num_to_shift)));
+}
+
+size_t BitArrCountOn(bit_array_t arr)
+{
+	size_t on_bits = 0;
+	
+	for(; arr; arr &= (arr - 1))
+	{
+		++on_bits;
+	}
+	
+	return (on_bits);
+}
+
+size_t BitArrCountOff(bit_array_t arr)
+{
+	size_t on_bits = 0;
+	
+	for(; arr; arr &= (arr - 1))
+	{
+		++on_bits;
+	}
+	
+	return (NUM_OF_BIT - on_bits);
+}
+
+bit_array_t BitArrMirror(bit_array_t arr)
+{
+	if(8 == sizeof(size_t))
+	{
+		arr = (arr >> 32) | (arr << 32);
+	
+		arr = (((arr & 0x0000FFFF0000FFFF) << 16)) | 
+			  (((arr & 0xFFFF0000FFFF0000) >> 16));
+
+		arr = (((arr & 0x00FF00FF00FF00FF) << 8)) | 
+			  (((arr & 0xFF00FF00FF00FF00) >> 8));
+
+		arr = (((arr & 0x0F0F0F0F0F0F0F0F) << 4)) | 
+			  (((arr & 0xF0F0F0F0F0F0F0F0) >> 4));
+
+		arr = (((arr & 0x3333333333333333) << 2)) | 
+			  (((arr & 0xCCCCCCCCCCCCCCCC) >> 2));
+
+		arr = (((arr & 0x5555555555555555) << 1)) | 
+			  (((arr & 0xAAAAAAAAAAAAAAAA) >> 1));
+	}
+	else
+	{
+		arr = ((arr << 16) | (arr >> 16));
+
+		arr = (((arr & 0x00FF00FF) << 8)) | (((arr & 0xFF00FF00) >> 8));
+
+		arr = (((arr & 0x0F0F0F0F) << 4)) | (((arr & 0xF0F0F0F0) >> 4));
+
+		arr = (((arr & 0x33333333) << 2)) | (((arr & 0xCCCCCCCC) >> 2));
+
+		arr = (((arr & 0x55555555) << 1)) | (((arr & 0xAAAAAAAA) >> 1));
+	}
+	
+	return (arr);
+}
+
+char *BitArrToString(bit_array_t arr, char *dest)
+{
+	char *curr = dest;
+	size_t mask = (size_t)1 << MAX_SHIFT;
+
+	while (mask)
+	{
+		*(curr++) = !!(mask & arr) + '0'; /* writes the current bit */
+		
+		mask >>= 1;
+	}
+	
+	*curr = '\0';	
+
+	return (dest);
+}
+
+size_t BitArrCountOnLUT(bit_array_t arr)
+{
+	size_t bits_on = 0;
+	size_t i = 0;
+	unsigned char *p_arr = (unsigned char*)&arr;
+	
+	for(i = 0; i < WORD_SIZE; ++i)
+	{
+		bits_on += bit_lut_g[*p_arr];
+
+		++p_arr;
+	}
+
+	return (bits_on);
+}
+
+bit_array_t BitArrMirrorLUT(bit_array_t arr)
+{
+	bit_array_t mirror_arr = 0;
+	unsigned char *curr_arr = (unsigned char*)&arr;
+	unsigned char *curr_mirror = (unsigned char*)&mirror_arr + WORD_SIZE - 1;
+	size_t i = 0;
+
+	for (i = 0; i < (WORD_SIZE / 2); ++i)
+	{
+		*curr_mirror = mirror_lut_g[*curr_arr];
+
+		++curr_arr;
+		--curr_mirror;
+	}
+
+	return (mirror_arr);
+}
+
+void InitBitLUT(void)
+{
+	size_t num = 0;
+	size_t i = 0;
+	size_t on_bits = 0;
+
+	for(i = 0; i < CHAR_VALS; ++i)
+	{
+		num = i;
+		on_bits = 0;		
+
+		for(; num; num &= (num - 1))
+		{
+			++on_bits;
+		}
+
+		bit_lut_g[i] = on_bits;
+	}	
+}
+
+void InitMirrorLUT(void)
+{
+	size_t i = 0;
+	unsigned char mirror_byte = 0;
+
+	for(i = 0; i < CHAR_VALS; ++i)
+	{
+		mirror_byte = i;
+
+		mirror_byte = (mirror_byte >> 4) | (mirror_byte << 4);
+
+		mirror_byte = (((mirror_byte & 0x33) << 2)) | (((mirror_byte & 0xcc) >> 2));
+
+		mirror_byte = (((mirror_byte & 0x55) << 1)) | (((mirror_byte & 0xaa) >> 1));
+
+		mirror_lut_g[i] = mirror_byte;
+	}
+}
---
+++ ds/ilrd_bit_array_test.c
@@ -0,0 +1,338 @@
+/*********************
+File name: bit_array_test.c
+Author: Ido finkelsein
+Reviewer: Uriel Naiman
+Date: 2/6/2020
+**********************/
+
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* size_t */
+#include "ilrd_bit_array.h"
+
+#define MAX_SHIFT (sizeof(size_t) * 8 - 1)
+
+/* testing function declarations */
+void BitArrSetAllTest(void);
+void BitArrResetAllTest(void); 
+void BitArrSetOnTest(void);
+void BitArrSetOffTest(void);
+void BitArrSetBitTest(void);
+void BitArrFlipTest(void);
+void BitArrIsOnTest(void);
+void BitArrIsOffTest(void);
+void BitArrRotRTest(void);
+void BitArrRotLTest(void);
+void BitArrCountOnTest(void);
+void BitArrCountOnLUTTest(void);
+void BitArrCountOffTest(void);
+void BitArrMirrorTest(void);
+void BitArrMirrorLUTTest(void);
+void BitArrToStringTest(void);
+void PrintBit(size_t num);
+void InitBitLUT(void);
+void InitMirrorLUT(void);
+
+int main()
+{
+	InitBitLUT();
+	InitMirrorLUT();
+	BitArrSetAllTest();
+	BitArrResetAllTest();
+	BitArrSetOnTest();
+	BitArrSetOffTest();
+	BitArrSetBitTest();
+	BitArrFlipTest();
+	BitArrIsOnTest();
+	BitArrIsOffTest();
+	BitArrRotRTest();
+	BitArrRotLTest();
+	BitArrCountOnTest();
+	BitArrCountOffTest();
+	BitArrMirrorTest();
+	BitArrToStringTest();
+
+	return 0;
+}
+
+void BitArrSetAllTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrSetAll ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrSetAll(arr);
+	
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	
+	puts("");
+}
+
+void BitArrResetAllTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrResetAll ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrResetAll(arr);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+	puts("");
+}
+
+void BitArrSetOnTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrSetOn ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrSetOn(arr, 50);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrSetOffTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrSetOff ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrSetOff(arr, 4);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrSetBitTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrSetBit ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrSetBit(arr, 17, 1);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrFlipTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrFlip ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrFlip(arr, 2);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrIsOnTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrIsOn ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrIsOn(arr, 1);
+
+	BitArrToString(arr, str);
+
+	printf("%ld\n", arr);
+
+	puts("");
+}
+
+void BitArrIsOffTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrSetOff ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrIsOff(arr, 1);
+
+	printf("%ld\n", arr);
+	puts("");
+}
+
+void BitArrRotRTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrRotR ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrRotR(arr, 3);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrRotLTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrRotL ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	arr = BitArrRotL(arr, 129);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	puts("");
+}
+
+void BitArrCountOnTest(void)
+{
+	bit_array_t arr = ~0;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrCountOn ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	printf("%ld\n", BitArrCountOn(arr));
+	printf("%ld\n", BitArrCountOnLUT(arr));
+
+	puts("");
+}
+
+void BitArrCountOffTest(void)
+{
+	bit_array_t arr = 4365;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrCountOff ***");
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+
+	printf("%ld\n", BitArrCountOff(arr));
+
+	puts("");
+}
+
+void BitArrMirrorTest(void)
+{
+	bit_array_t arr = 4365095;
+	bit_array_t arr2 = 4365095;
+	char str[65] = {0};
+
+	puts("*** testing  BitArrMirror ***");
+
+	BitArrToString(arr, str);
+	BitArrToString(arr2, str);
+
+	arr = BitArrMirror(arr);
+	arr2 = BitArrMirrorLUT(arr2);
+	
+	BitArrToString(arr, str);
+	printf("%s\n", str);
+
+	BitArrToString(arr2, str);
+	printf("%s\n", str);
+	
+	puts("");
+}
+
+void BitArrToStringTest(void)
+{
+	char str[65] = {0};
+	
+	bit_array_t arr = 0x796B6E69466F6449;
+
+	puts("*** testing  BitArrToString ***");
+
+	PrintBit(arr);
+
+	BitArrToString(arr, str);
+
+	printf("%s\n", str);
+	puts("");
+}
+
+void PrintBit(bit_array_t num)
+{
+	size_t mask = (size_t)1 << MAX_SHIFT;
+
+	while (mask)
+	{
+		printf("%d", !!(mask & num)); 
+		
+		mask >>= 1;
+	}
+
+	puts("");
+}
