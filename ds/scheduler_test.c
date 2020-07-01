e4525d1f641c1815631a426a23391ce2785e6c0b
ido.finkelstein <finkelsteinido@gmail.com>
2020-07-01 08:52:03 +0300

    sched

--- ds/scheduler_test.c
+++ ds/scheduler_test.c
@@ -3,6 +3,37 @@
 
 #include "scheduler.h"
 
+int task1(sch_t *sch, unique_id_t uid, void *param);
+int task2(sch_t *sch, unique_id_t uid, void *param);
+int task3(sch_t *sch, unique_id_t uid, void *param);
+
+int main()
+{
+	sch_t *sched = SchCreate();
+	sch_task_func_t task_func = task1;
+	unique_id_t uid1 = SchTimerStart(sched, 1, task_func , NULL);
+	unique_id_t bad_uid = {0, -1, -1};
+	unique_id_t uid2;
+
+	SchTimerStart(sched, 2, task3, NULL);
+	printf("%ld-%d-%ld\n", uid1.time, uid1.pid, uid1.counter);
+	uid2 = SchTimerStart(sched, 2, task2, NULL);
+	
+	printf("%ld-%d-%ld\n", uid2.time, uid2.pid, uid2.counter);
+	uid1 = SchTimerStart(sched,3, task_func , NULL);
+	
+	printf("%ld-%d-%ld\n", uid1.time, uid1.pid, uid1.counter);
+
+	SchTimerCancel(sched, uid2);
+	SchTimerCancel(sched, bad_uid);
+
+	SchRun(sched);
+
+	SchDestroy(sched);
+
+	return 0;
+}
+
 int task1(sch_t *sch, unique_id_t uid, void *param)
 {
 	static size_t count = 0;
@@ -45,29 +76,4 @@ int task3(sch_t *sch, unique_id_t uid, void *param)
 	return 0;
 }
 
-int main()
-{
-	sch_t *sched = SchCreate();
-	sch_task_func_t task_func = task1;
-	unique_id_t uid1 = SchTimerStart(sched, 1, task_func , NULL);
-	unique_id_t bad_uid = {0, -1, -1};
-	unique_id_t uid2;
 
-	SchTimerStart(sched, 2, task3, NULL);
-	printf("%ld-%d-%ld\n", uid1.time, uid1.pid, uid1.counter);
-	uid2 = SchTimerStart(sched, 2, task2, NULL);
-	
-	printf("%ld-%d-%ld\n", uid2.time, uid2.pid, uid2.counter);
-	uid1 = SchTimerStart(sched,3, task_func , NULL);
-	
-	printf("%ld-%d-%ld\n", uid1.time, uid1.pid, uid1.counter);
-
-	SchTimerCancel(sched, uid2);
-	SchTimerCancel(sched, bad_uid);
-
-	SchRun(sched);
-
-	SchDestroy(sched);
-
-	return 0;
-}
