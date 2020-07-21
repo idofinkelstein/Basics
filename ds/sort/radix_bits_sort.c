9f427a20e5158da92816c6c07f3d87147bac44f2
ido.finkelstein <finkelsteinido@gmail.com>
2020-07-21 13:59:33 +0300

    new version

--- ds/sort/radix_bits_sort.c
+++ ds/sort/radix_bits_sort.c
@@ -1,8 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+/***********************
+File name: radix_bits_sort.c
+Author: Ido Finkelstein
+Reviewer: Sharazad Tarabush
+Date: 20/7/2020
+************************/
 
-
+#include <stdlib.h> /* calloc, free */
+#include <string.h> /* memcpy       */
 
 #include "radix_sort.h"
 
@@ -15,7 +19,7 @@ void CountingSortHelper(uint *arr,
 					    uint *count_arr,
 					    size_t range,					
 					    uint to_mask,
-						uint chank,
+						uint chunk,
 					    uint min);
 
 int RadixBitsSort(uint *arr, size_t size, size_t num_of_bits)
@@ -25,8 +29,8 @@ int RadixBitsSort(uint *arr, size_t size, size_t num_of_bits)
 	unsigned *buffer = (unsigned*)calloc(size, sizeof(unsigned));
 	size_t i = 0;
 	uint mask = (uint)((1ul << num_of_bits) - 1);
-	uint min = 0;
-	uint chank = 0;
+	uint min = 0; /* unrelevant in this function */
+	uint chunk = 0;
 
 	if (NULL == count_arr)
 	{
@@ -40,33 +44,27 @@ int RadixBitsSort(uint *arr, size_t size, size_t num_of_bits)
 		return (MALLOC_FAILED);
 	}
 
-	
 	while (mask)
 	{
-		printf("mask = %u\n", mask);
-
-		if (!(i & 1))
+		if (!(i & 0x1)) /* if even */
 		{
-			CountingSortHelper(arr, buffer, size, count_arr, range, mask, chank, min);
+			CountingSortHelper(arr, buffer, size, count_arr, 
+							   range, mask, chunk, min);
 		}
 		else
 		{
-			CountingSortHelper(buffer, arr, size, count_arr, range, mask, chank, min);
-		}/**/
+			CountingSortHelper(buffer, arr, size, count_arr, 
+							   range, mask, chunk, min);
+		}
 		
 		mask <<= num_of_bits;
 		
+		chunk += num_of_bits;
+
 		++i;
-		printf("chank = %u\n", chank);
-		chank += num_of_bits;
-	
 	}
 
-	printf("i = %ld\n", i);
-	
-	printf("range = %u\n",range);
-
-	if (i & 1)	
+	if (i & 0x1) /* if odd */
 	{
 		memcpy(arr, buffer, size * sizeof(uint));
 	}
@@ -77,14 +75,12 @@ int RadixBitsSort(uint *arr, size_t size, size_t num_of_bits)
 	return (SUCCESS);
 }
 
-
 int CountingSort(uint *arr, size_t size, uint min, uint max)
 {
 	size_t range = max - min + 1;
 	uint *count_arr = (unsigned*)calloc(range, sizeof(unsigned));
 	uint *result_arr = (unsigned*)calloc(size, sizeof(unsigned));
-	
-	uint to_mask = ~0;
+	uint to_mask = ~0; /* constant value in this function */
 
 	if (NULL == count_arr)
 	{
@@ -102,7 +98,6 @@ int CountingSort(uint *arr, size_t size, uint min, uint max)
 
 	memcpy(arr, result_arr, size * sizeof(uint));
 
-
 	free(count_arr);
 	free(result_arr);
 
@@ -115,7 +110,7 @@ void CountingSortHelper(uint *arr,
 					    uint *count_arr,
 					    size_t range,					
 					    uint to_mask,
-						uint chank,
+						uint chunk,
 					    uint min)
 {
 	size_t i = 0;
@@ -125,7 +120,7 @@ void CountingSortHelper(uint *arr,
 
 	for (i = 0; i < size; ++i)
 	{
-		++count_arr[((arr[i] & to_mask) >> chank) - shift]; 
+		++count_arr[((arr[i] & to_mask) >> chunk) - shift]; 
 	}
 
 	for (i = 1; i < range; ++i)
@@ -134,27 +129,19 @@ void CountingSortHelper(uint *arr,
 	}
 
 	for (i = 0; i < size; ++i)
-	{	
-		/* goes to arr[i] starting in the last cell, takes its value, this
-		   value after shifting will be index to the count_arr, the value
-		   of count_arr after subtruting 1 will be the index in the result 
-		   array that will contain the value of arr[i].  
+	{			
+		to_place = arr[size - i - 1];
 
-		*/
-		
-			to_place = arr[size - i - 1];
+		place = count_arr[((to_place & to_mask) >> chunk)- shift];
 
-			place = count_arr[((to_place & to_mask) >> chank)- shift];
+		buffer[place - 1] = to_place;
 
-			buffer[place - 1] = to_place;
-
-			--count_arr[((to_place & to_mask) >> chank)- shift];
+		--count_arr[((to_place & to_mask) >> chunk)- shift];
 	}
 
 	for (i = 0; i < range; ++i)
 	{
 		count_arr[i] = 0;
 	}
-
 
 }
--- ds/sort/radix_sort_test.c
+++ ds/sort/radix_sort_test.c
@@ -1,12 +1,34 @@
-#include <stdio.h>
+/***********************
+File name: radix_sort_test.c
+Author: Ido Finkelstein
+Reviewer: Sharazad Tarabush
+Date: 20/7/2020
+************************/
+
+#include <stdio.h> /* printf */
 
 #include "radix_sort.h"
 
+void CountingSortTest(void);
+void RAdixSortTest(void);
+
 int main()
 {
-	uint arr[] = {3, 5, 10, 9, 4, 6/*, 3, 11, 3, 4, 3, 12, 7*/};
+	CountingSortTest();
+	RAdixSortTest();
+
+	return 0;
+}
+
+void CountingSortTest(void)
+{
+	uint arr[] = {3, 5, 10, 9, 4, 6, 3, 11, 3, 4, 3, 12, 7,
+				  44, 2, 32, 23, 19, 20, 8, 11, 3, 14, 13, 76};
 	size_t size = sizeof(arr) / sizeof(int);
 	size_t i = 0;
+
+	puts("\n*** testing counting sort ***");
+	puts("unsorted:");
 
 	for (i = 0; i < size; ++i)
 	{
@@ -15,9 +37,9 @@ int main()
 
 	puts("");
 
+	CountingSort(arr, size, 2, 76);
 
-	/*CountingSort(arr, size, 3, 12);*/
-	RadixBitsSort(arr, size, 2);
+	puts("sorted:");
 	
 	for (i = 0; i < size; ++i)
 	{
@@ -25,7 +47,33 @@ int main()
 	}
 
 	puts("");
+}
 
+void RAdixSortTest(void)
+{
+	uint arr[] = {3, 5, 10, 9, 4, 6, 3, 11, 3, 4, 3, 12, 7,
+				  44, 2, 32, 23, 19, 20, 8, 11, 3, 14, 13, 76};
+	size_t size = sizeof(arr) / sizeof(int);
+	size_t i = 0;
 
-	return 0;
+	puts("\n*** testing radix sort ***");
+	puts("unsorted:");
+
+	for (i = 0; i < size; ++i)
+	{
+		printf("%u, ", arr[i]);
+	}
+
+	puts("");
+
+	RadixBitsSort(arr, size, 11);
+	
+	puts("sorted:");
+
+	for (i = 0; i < size; ++i)
+	{
+		printf("%u, ", arr[i]);
+	}
+
+	puts("");
 }
