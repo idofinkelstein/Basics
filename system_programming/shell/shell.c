3738f91f213f239f34f84942f9306e1aa9417163
ido.finkelstein <finkelsteinido@gmail.com>
2020-08-31 11:29:42 +0300

    q

--- system_programming/shell/shell.c
+++ system_programming/shell/shell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>		/* exit */
 #include <string.h>		/* strtok, strcmp */
 #include <signal.h>		/* signal */
+#include <stdbool.h>	/* true, false */
 #include <sys/types.h>	/* fork */
 #include <sys/wait.h>   /* wait */
 #include <unistd.h>		/* exec, fork */
@@ -23,6 +24,12 @@ void CleanUp(char **arg);
 void Shell(void)
 {
 	char input[200];
+	pid_t pid = 0;
+	int status = 0;
+	int background = false;
+	size_t len = 0;
+	char **arg = NULL;
+
 
 	if (signal(SIGINT, SIG_IGN) == SIG_ERR)
 	{
@@ -30,51 +37,60 @@ void Shell(void)
 		exit(EXIT_FAILURE);
 	}
 
-
 	while (1)	
 	{
 		scanf("%[^\n]%*c", input);
-		CreateProcess(input);
+
+		len = strlen(input);
+
+		if (0 == strcmp(input, "exit"))
+		{
+			exit(0);
+		}
+
+		if ('&' == *(input + len - 1))
+		{
+			background = true;
+			*(input + len - 1) = '\0';
+		}
+
+		arg = BreakInput(input);
+	
+		/* fork() returns the process identifier (pid) of the child process in the
+ 		   parent, and returns 0 in the child. */
+		pid = fork();
+
+		if (-1 == pid)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		if (pid == 0)
+		{			
+			/* goes in only from the child process */
+			execvp(arg[0], arg);	
+			exit(0);
+		}
+		else
+		{
+			if (background)
+			{
+				waitpid(pid,&status, WNOHANG);
+				background = !background;
+			}
+			else
+			{
+				waitpid(pid,&status, 0);
+				
+			}
+		}
+	
+		free(arg);
 	}
 }
 /*---------------------------------------------------------------------------*/
 /* Utils functions definitions */
 /*---------------------------------------------------------------------------*/
-void CreateProcess(char *input)
-{
-	pid_t pid = 0;
-	int status = 0;
-	
-	char **arg = BreakInput(input);
-	
-	/* fork() returns the process identifier (pid) of the child process in the
- 	   parent, and returns 0 in the child. */
-	pid = fork();
-
-	if (0 == strcmp(input, "exit"))
-	{
-		exit(0);
-	}
-	;;;
-
-	if (-1 == pid)
-	{
-		exit(EXIT_FAILURE);
-	}
-
-	if (pid == 0)
-	{			
-		/* goes in only from the child process */
-		execvp(arg[0], arg);	
-		exit(0);
-	}
-	else
-	{
-		wait(&status);
-	}
-	
-	free(arg);
-}
 
 /*----------------------------------------------------------------------------*/
 
@@ -84,7 +100,7 @@ char **BreakInput(char *input)
 	char *str = NULL;
 	char **start = NULL;
 
-	arg = (char**)malloc(sizeof(char*) * 10);
+	arg = (char**)malloc(sizeof(char*) * 20);
 
 	if(NULL == arg)
 	{
@@ -94,8 +110,7 @@ char **BreakInput(char *input)
 	start = arg;
 
 	str = strtok(input, " ");
-	
- 
+	 
    /* walk through other tokens */
 	while(str != NULL)
 	{
