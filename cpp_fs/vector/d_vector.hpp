59cdea2f86c320678e20329967c5408e777030ba
ido.finkelstein <finkelsteinido@gmail.com>
2020-10-31 12:07:31 +0200

    vector 1.0

--- cpp_fs/vector/d_vector.hpp
+++ cpp_fs/vector/d_vector.hpp
@@ -7,17 +7,18 @@ Template class - can hold any types as long as they support:
 Main operations on vector:
     -   Push back - adds an element to the end of the vector
     -   Pop back - removes the last element in the vector
-    -   get capacity - returns the current capacity of the vector
-    -   get size - returns the current number of elements in the vector
+    -   get m_capacity - returns the current m_capacity of the vector
+    -   get m_size - returns the current number of elements in the vector
     -   operator[] const- retrieve element from specific index (read only)
     -   operator[] - retrieve access to specific index (read / write)
-    -   resize - reallocates and initializes memory to the new capacity
-    -   reserve - reallocates uninitialized memory to the new capacity
+    -   resize - reallocates and initializes memory to the new m_capacity
+    -   reserve - reallocates uninitialized memory to the new m_capacity
 */
 #pragma once
 
 #include <cstring>
 #include <cstddef> //size_t
+#include <cassert>
 
 namespace ilrd
 {
@@ -39,61 +40,145 @@ public:
     explicit DVector(size_t init_size = 0);
 
     ~DVector();
-    void PushBack(const T& data);
+    void PushBack(const T& m_data);
     void PopBack();
-    size_t Size() const;
-    size_t Capacity() const;
+    inline size_t Size() const;
+    inline size_t Capacity() const;
     T& operator[](size_t index);
     const T& operator[](size_t index) const;
     void Resize(size_t new_size, T t = T());
-    void Reserve(size_t new_size);
+    void Reserve(size_t new_capacity);
 
 private:
     DVector(const DVector& other);
     DVector& operator=(const DVector& other);
+	void ChangeCapacity(size_t size);
 
-    size_t capacity;
-    size_t size;
-    T *data;    // array of T members
+    size_t m_capacity;
+    size_t m_size;
+    T *m_data;    // array of T members
 };
 
 
 template <typename T>
-DVector<T>::DVector(size_t init_size) : capacity(init_size),
-                                        size(init_size),
-                                        data(new T[capacity])
-
-{
-        
-}
+DVector<T>::DVector(size_t init_size) : m_capacity(init_size * RESIZE_FACTOR + 1),
+                                        m_size(init_size),
+                                        m_data(new T[m_capacity]) {}
 
 template <typename T>
 DVector<T>::~DVector()
 {
-    delete[] data;     
+    delete[] m_data;     
 }
 
 template <typename T>
 void DVector<T>::PushBack(const T& data)
 {
-    ++size;
-
-    if(size >= capacity)
+    if(m_size >= m_capacity)
     {
-        T *tmp_data = new T[capacity * INC_FACTOR];
-        memcpy(tmp_data, this->data, (size - 1) * sizeof(T));
-
-        delete[] this->data;
-        this->data = tmp_data;
-
-        capacity *= 2;
+		ChangeCapacity(m_capacity * INC_FACTOR);
     }
 
-    this->data[size - 1] = data;
-
+    ++m_size;
+    this->m_data[m_size - 1] = data;
 }
 
+template <typename T>
+ void DVector<T>::PopBack()
+{
+	if (0 != m_size)
+	{
+		--m_size;
+	}
+	else
+	{
+		return;
+	}
 
+	// reduces capacity by 2 in case of size < (current capacity / 4) 
+	if (m_size < (m_capacity / RESIZE_FACTOR)) 
+	{
+		ChangeCapacity(m_capacity / DEC_FACTOR); 
+	}
+}
 
+template <typename T>
+inline size_t DVector<T>::Size() const
+{
+	return m_size;
+}
+
+template <typename T>
+inline size_t DVector<T>::Capacity() const
+{
+	return m_capacity;
+}
+
+template <typename T>
+T& DVector<T>::operator[](size_t index)
+{
+	assert(index < m_size);
+
+	return (const_cast<T&>( static_cast<const DVector&>(*this)[index]) );
+}
+
+template <typename T>
+const T& DVector<T>::operator[](size_t index) const
+{
+	return m_data[index];
+}
+
+template <typename T>
+void DVector<T>::Reserve(size_t new_capacity)
+{
+	if (new_capacity > this->Capacity)
+	{
+		ChangeCapacity(new_capacity);
+	}
+}
+
+template <typename T>
+void DVector<T>::ChangeCapacity(size_t size)
+{
+	T *tmp_data = new T[size];
+
+	for(size_t i = 0; i < m_size; ++i)
+	{
+		tmp_data[i] =  this->m_data[i];
+	}
+
+    delete[] this->m_data;
+    this->m_data = tmp_data;
+
+	m_capacity = size;
+}
+
+template <typename T>
+void DVector<T>::Resize(size_t new_size, T t)
+{
+	size_t start = 0, end = 0;
+
+	if (new_size < Size())
+	{
+		m_size = new_size;
+	}	
+	else if (new_size > Size() && new_size <= Capacity())
+	{
+		start = Size();
+		end = m_size = new_size;
+	}
+	else if (new_size > Capacity())
+	{
+		start = Size();
+		ChangeCapacity(new_size);
+		m_size = new_size;
+		end = Capacity();
+	}
+
+	for (size_t i = start; i < end; ++i)
+	{
+		m_data[i] = t;
+	}
+}
 } // namespace rd90
 } // namespace ilrd
--- cpp_fs/vector/d_vector_test.cpp
+++ cpp_fs/vector/d_vector_test.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 
 #include "d_vector.hpp"
+#include "rc_string.hpp"
 
 using namespace ilrd::rd90;
 
+void TestStringMembers(void);
+void TestIntMembers(void);
 int main()
-{
-    DVector<int> v1(4);
+{  
+	TestStringMembers();
+	TestIntMembers();
 
-    for(size_t i = 0; i < 100; ++i)
-    {
-        v1.PushBack(5);
-    }
     return 0;
+}
+
+void TestStringMembers(void)
+{
+	DVector<RCString>v(1);
+	std::cout << "capacity = " << v.Capacity() << std::endl;
+	for(size_t i = 0; i < 100; ++i)
+	v.PushBack("hello");
+
+	v[5] = v[5] + " world";
+
+	v[12] = v[5];
+
+	v[99] = v[13] = v[5];
+
+	std::cout << "capacity = " << v.Capacity() << std::endl;
+
+	for(size_t i = 0; i < 100; ++i)
+	std::cout << v[i] << std::endl;
+
+	for(size_t i = 0; i < 80; ++i)
+	v.PopBack();
+
+	std::cout << "capacity = " << v.Capacity() << std::endl;
+	std::cout << "size = " << v.Size() << std::endl;
+
+	v.Resize(300, "bye");
+
+	for(size_t i = 0; i < v.Size(); ++i)
+	std::cout << v[i] << i << std::endl;
+
+}
+
+void TestIntMembers(void)
+{
+	DVector<int> v(4);
+
+	for(size_t i = 0; i < 100; ++i)
+    {
+        v.PushBack(5);
+    }
 }
