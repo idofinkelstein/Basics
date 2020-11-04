c21a5829b5e4b6c83dbf2bc2b3e2eb8f846c0a87
ido.finkeltein <finkelsteinido@gmail.com>
2020-11-04 12:27:34 +0200

    aa

--- system_programming/ext2/ext2.c
+++ system_programming/ext2/ext2.c
@@ -18,6 +18,7 @@ int main()
 	int ram_fd = 0;
 	char sb_buff[1024];
 	int status = 0;
+	unsigned int group_count;
 
 	ram_fd = open("/dev/ram0", O_RDONLY);
 	if (-1 == ram_fd)
@@ -26,17 +27,20 @@ int main()
 		return 1;
 	}
 
-    status = lseek(ram_fd, SECTOR_SIZE, SEEK_SET);
-	if (status != SECTOR_SIZE)
+    status = lseek(ram_fd, SECTOR_SIZE * 2, SEEK_SET);
+	if (status != SECTOR_SIZE * 2)
 	{
 		puts("could'nt seek");
 		printf("lseek: %d\n", status);
 		return 1;
 	}
-    read(ram_fd, sb_buff, SECTOR_SIZE);
+    read(ram_fd, sb_buff, SECTOR_SIZE * 2);
 
-	
+	sb = (struct ext2_super_block *)sb_buff;
+	printf("%d\n", sb->s_inodes_count); 
 
+	group_count = 1 + (sb->s_blocks_count-1) / sb->s_blocks_per_group;
+	printf("%d\n", group_count); 
 	
 	return 0;
 }
