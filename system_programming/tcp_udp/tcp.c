fc1422ac8ee95964531a25c0eb43c44b6546567a
ido.finkeltein <finkelsteinido@gmail.com>
2020-11-10 16:23:24 +0200

    adf

--- system_programming/tcp_udp/tcp.c
+++ system_programming/tcp_udp/tcp.c
@@ -3,7 +3,7 @@
  * Reviewer    : 
  * Review Date : 9.11.20
  */
-
+#define _DEFAULT_SOURCE
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -19,16 +19,23 @@
 
 int main(int argc, char const *argv[])
 {
-    int sockfd; 
+    int sockfd, new_fd; 
     char buffer[MAXLINE]; 
-    char *ping = "ping";
-    char *pong = "pong"; 
-    struct sockaddr_in server_addr, client_addr;
+    char *ping = "ping\n";
+    char *pong = "pong\n"; 
+    
     struct addrinfo hints;
     struct addrinfo *servinfo;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
     int status;
-    int n;
-    unsigned int len;
+    unsigned int len = strlen(ping);
+
+    if (argc != 2)
+    {
+        return 0;
+    }
+
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
@@ -36,20 +43,95 @@ int main(int argc, char const *argv[])
     hints.ai_flags = AI_PASSIVE;
 
 
-if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0)
+if ((status = getaddrinfo(NULL, "9000", &hints, &servinfo)) != 0)
 {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(1);
 }
 
     /* Creating socket file descriptor */
-    if (-1 == (sockfd = socket(AF_INET, SOCK_STREAM, 0))) /* TCP protocol */
+    if (-1 == (sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, 0))) /* TCP protocol */
     { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     }
 
+    if (0 == strcmp(argv[1], "server")) /* we are the server */
+    {
+        if (-1 == bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))
+        {
+            perror("bind"); 
+            exit(EXIT_FAILURE);
+        }
 
+        if (-1 == listen(sockfd, 5))
+        {
+            perror("listen"); 
+            exit(EXIT_FAILURE);
+        }
 
+        addr_size = sizeof(struct sockaddr_storage);
+
+        if (-1 == (new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)))
+        {
+            perror("accept"); 
+            exit(EXIT_FAILURE);
+        }
+
+        while (1)
+        {
+
+            if(-1 == recv(new_fd, buffer,len, 0))
+            {
+                perror("recv"); 
+                close(sockfd);
+                close(new_fd);
+                exit(EXIT_FAILURE);
+            }
+
+            sleep(1);
+            write(1, buffer, len);
+
+            if (-1 == send(new_fd, pong, len, 0))
+            {
+                perror("send");
+                close(sockfd);
+                close(new_fd);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    else if (0 == strcmp(argv[1], "client")) /* we are the client */
+    {
+        if (-1 == connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))
+        {
+            perror("connect");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        while(1)
+        {
+            if (-1 == send(sockfd, ping, len, 0))
+            {
+                perror("send");
+                close(sockfd);
+                exit(EXIT_FAILURE);
+            }
+
+            if(-1 == recv(sockfd, buffer,len, 0))
+            {
+                perror("recv"); 
+                close(sockfd);
+                exit(EXIT_FAILURE);
+            }
+
+            sleep(1);
+            write(1, buffer, len);
+        }
+
+        close(sockfd);
+
+    }
     return 0;
 }
