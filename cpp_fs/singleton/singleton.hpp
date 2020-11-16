30aa1e4640d2579a10b6a24d52b1f2b03288a046
ido.finkeltein <finkelsteinido@gmail.com>
2020-11-16 20:02:41 +0200

    version 1.1

--- cpp_fs/singleton/singleton.hpp
+++ cpp_fs/singleton/singleton.hpp
@@ -1,24 +1,36 @@
 #include <cstdlib>
 #include <atomic>
+#include <iostream>
+#include <pthread.h>
+
+namespace ilrd
+{
+namespace rd90
+{
 
 template<typename T>
 class Singleton
 {
 public:
-    explicit Singleton(/* args */);
+    explicit Singleton();
     ~Singleton();
 
     T* operator->();
     static void Cleanup();
+    void GetCount();
 
     static T* Instance();
 private:
     static T* instancePtr;
+    static int instance_count;
 };
 
 
 template<typename T>
 T* Singleton<T>::instancePtr = 0;
+
+template<typename T>
+int Singleton<T>::instance_count = 0;
 
 template<typename T>
 Singleton<T>::Singleton()
@@ -39,9 +51,16 @@ Singleton<T>::~Singleton()
 }
 
 template<typename T>
+void Singleton<T>::GetCount()
+{
+    std::cout << "num of instances = " << instance_count << std::endl;
+}
+
+template<typename T>
 void Singleton<T>::Cleanup()
 {
     delete (instancePtr);
+    instancePtr = reinterpret_cast<T*>(0xDEADBEEF);
 }
 
 
@@ -55,15 +74,16 @@ T* Singleton<T>::Instance()
         if (!__atomic_test_and_set(&initStarted, __ATOMIC_ACQUIRE))
         {
             __atomic_store_n(&instancePtr, new T, __ATOMIC_SEQ_CST);
+            ++instance_count;
 
             atexit(Cleanup);
         }
-        else while (__atomic_load_n(&instancePtr, __ATOMIC_SEQ_CST) == 0)
-        {
-            /* code */
-        }
-        
+        /* spin lock */
+        else while (__atomic_load_n(&instancePtr, __ATOMIC_SEQ_CST) == 0);       
     }
 
     return (instancePtr);
 }
+
+} // rd90
+} // ilrd
--- cpp_fs/singleton/test.cpp
+++ cpp_fs/singleton/test.cpp
@@ -1,17 +1,61 @@
 #include "singleton.hpp"
 
+using namespace ilrd::rd90;
+
 class A
 {
 public:
+    template<typename T>
+    friend class ilrd::rd90::Singleton;
+
     void Foo() {}
+private:
+    A(){}
 };
 
+void Foo();
+void *Action(void *arg);
+
 int main()
+{
+    pthread_t tid[4];
+
+    for (int i = 0; i < 4; ++i)
+    {
+        pthread_create(&tid[i], NULL, Action, NULL);
+    }
+
+    for (int i = 0; i < 4; ++i)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    Singleton<A> instance;
+    Singleton<A> instance2;
+
+    instance->Foo();
+    instance2->Foo();
+    Foo();
+    instance2.GetCount();
+    return 0;
+}
+
+
+void Foo()
 {
     Singleton<A> instance;
 
     instance->Foo();
+}
 
+void *Action(void *arg)
+{
+    (void)arg;
 
-    return 0;
+    Singleton<A> instance;
+
+    instance->Foo();
+    Foo();
+
+    return (NULL);
 }
