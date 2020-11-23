e54d91a9c0dfa5ca9c1f7d1212bcf18b8d09c791
ido.finkeltein <finkelsteinido@gmail.com>
2020-11-23 13:18:44 +0200

    ver 1.1

--- cpp_fs/functor/function.hpp
+++ cpp_fs/functor/function.hpp
@@ -9,58 +9,78 @@ namespace rd90
 class Function
 {
 public:
+    // Free function ctor
+    explicit Function(void(fptr)(int));
+
+    // Member function ctor
     template <typename T>
-    explicit Function(void(T::*m_fptr)(int a), T *t);
+    Function(void(T::*fptr)(int a), T *t);
+
     void operator()(int);
 
 class I_Function
 {
-//public:    
+public:    
     virtual void operator()(int) = 0;
-    virtual ~I_Function();
+    virtual ~I_Function() = default;
 };
     
-
-
 template <typename T>
-class FunctionProxy : public I_Function
+class MemberFunctionProxy : public I_Function
 {
 public:
     void operator()(int);
-    FunctionProxy(void(T::*m_fptr)(int), T *t);
+    MemberFunctionProxy(void(T::*fptr)(int), T *t);
 private:
-    T *m_obj;
     void(T::*m_func)(int);
+    T *m_obj;
+};    
+
+class FreeFunctionProxy : public I_Function
+{
+public:
+    void operator()(int);
+    FreeFunctionProxy(void(fptr)(int));
+private:
+    void(*m_func)(int);
 };    
 
 private:
     std::shared_ptr<I_Function> m_ptr;  
 };
 
-void Function::operator()(int)
+void Function::operator()(int val)
 {
-    
+    (*m_ptr)(val);
 }
 
 template <typename T>
-Function::Function(void(T::*m_fptr)(int), T *t)
-: m_ptr(new FunctionProxy<T>(m_fptr, t)) {}
+Function::Function(void(T::*fptr)(int), T *t)
+: m_ptr(new MemberFunctionProxy<T>(fptr, t)) {}
+
+Function::Function(void(fptr)(int))
+: m_ptr(new FreeFunctionProxy(fptr)) {}
 
 
 template <typename T>
-Function::FunctionProxy<T>::FunctionProxy(void(T::*m_fptr)(int), T *t)
-: m_func(m_fptr), 
+Function::MemberFunctionProxy<T>::MemberFunctionProxy(void(T::*fptr)(int), T *t)
+: m_func(fptr), 
   m_obj(t) {}
 
 
 template <typename T>
-void Function::FunctionProxy<T>::operator()(int num)
+void Function::MemberFunctionProxy<T>::operator()(int num)
 {
     (m_obj->*m_func)(num);  
 }
 
+Function::FreeFunctionProxy::FreeFunctionProxy(void(fptr)(int)) : m_func(fptr)
+{}
 
-
+void Function::FreeFunctionProxy::operator()(int val)
+{
+    m_func(val);
+}
 
 
 } // namespace rd90
--- cpp_fs/functor/rd90.cpp
+++ cpp_fs/functor/rd90.cpp
@@ -11,13 +11,18 @@ using namespace ilrd::rd90;
 
 void for_each(int* vector, size_t size,  Function action)
 {
-    for (int i=0; i < size; ++i)
+    for (size_t i = 0; i < size; ++i)
     {
        action(vector[i]);
     }
 
 }
 
+void Foo(int val)
+{
+    std::cout << "free function Foo" << std::endl;
+    std::cout << "the value is: " << val << std::endl;
+}
 
 // -------------------- user code
 class Widget
@@ -25,12 +30,12 @@ class Widget
 public:
     void Action1(/* Widget* const this, */ int data)
     {
-        std::cout << data << std::endl;
+        std::cout << "widget action1 " << data << std::endl;
     }
 
     void Action2(/* Widget* const this, */ int data)
     {
-        std::cout << data << std::endl;
+        std::cout << "widget action2 " << data << std::endl;
     }
 };
 
@@ -39,7 +44,7 @@ class Gadget
 public:
     void Action(/* Gadget* const this, */ int data)
     {
-        std::cout << data << std::endl;
+        std::cout << "gadget action " << data << std::endl;
     }
 };
 
@@ -50,12 +55,12 @@ int main()
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     Widget w1;
     Gadget g1;
-
+    Function f1(Foo);
 
     for_each(arr, 10, Function(&Widget::Action1, &w1));
     for_each(arr, 10, Function(&Widget::Action2, &w1));
     for_each(arr, 10, Function(&Gadget::Action, &g1));
 
-
+    f1(10);
     return 0;
 }
