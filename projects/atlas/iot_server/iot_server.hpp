2435e50cb266f450c575426452275c90e2956bcc
ido.finkelstein <finkelsteinido@gmail.com>
2021-01-12 13:33:16 +0200

    iot server construction

--- projects/atlas/iot_server/iot_server.hpp
+++ projects/atlas/iot_server/iot_server.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <cstdio> 
+#include <cstdlib> 
+#include <unistd.h> 
+#include <cstring>
+#include <string> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h>
+#include <netdb.h>
+
+const char *defaultPort = "29000;";
+
 namespace ilrd
 {
 namespace rd90
@@ -8,8 +21,18 @@ namespace rd90
 class Server
 {
 public:
+    explicit Server();
+
+    Server(const Server& other) = delete;
+    void operator=(const Server& other) = delete;
+
+    void EstablishConnection(std::string port = defaultPort);
+    void Run();
 
 private:
+    int m_sockFd;
+    int m_communicationFd;
+    static int globalPort;
 
 
 };
